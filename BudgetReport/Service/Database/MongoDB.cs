009bb1d07fdf31201a255f69fc7597328a222f1d
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-24 16:48:34 +0500

    Initialize Serive

---
+++ BudgetReport/Service/Database/MongoDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetReport.Service.Database
+{
+    internal class MongoDB : IDatabase
+    {
+        public void Delete(int lineNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int lineNumber, string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BudgetReport/Service/Report/IReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetReport.Service.Report
+{
+    internal interface IReport
+    {
+        void Open(string date);
+        void Save();
+    }
+}
